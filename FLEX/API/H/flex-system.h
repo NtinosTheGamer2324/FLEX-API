157f11825c54c5b88490bb1809927139b59f01ab
NTS STUDIOS <115401559+NtinosTheGamer2324@users.noreply.github.com>
2025-05-08 00:06:07 +0300

    Update flex-system.h

--- FLEX/API/H/flex-system.h
+++ FLEX/API/H/flex-system.h
@@ -1,21 +1,25 @@
 #ifndef FLEX_SYSTEM_H
 #define FLEX_SYSTEM_H
 
-// Initialization Functions
+// System Initialization
 void system_init(void);
 void system_start(void);
 void system_halt(void);
-void system_print(const char* msg);
 
-// Hardware Functions
+// Hardware Interfaces
 void system_init_hardware(void);
+void system_print(const char *msg);
 
-// Keyboard, Mouse, and Audio Drivers
+// Keyboard Driver
 void keyboard_init(void);
 int keyboard_read(void);
+
+// Mouse Driver
 void mouse_init(void);
-void mouse_read(int* x, int* y, int* button_status);
+void mouse_read(int *x, int *y, int *button_status);
+
+// Audio Driver
 void audio_init(void);
-void audio_play_sound(const char* sound_file);
+void audio_play_sound(uint16_t frequency, uint32_t duration);
 
 #endif // FLEX_SYSTEM_H
